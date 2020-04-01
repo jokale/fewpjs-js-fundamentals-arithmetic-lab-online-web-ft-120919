d72d29d1f16080c3a988d42586dbd439828eb16c
jokale <kalemajoanna@gmail.com>
2020-04-01 18:02:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // Write your code here
+
+let num1 = 31 
+let num2 = 2 
+
+let multiply = num1*num2
+
+function multiply(num1, num2){
+    return multiply
+}
+
+
+
+let random = 1
+
+  function random(){
+     Math.random()
+ };
+
+
+let mod = 4
+
+function mod(){
+    mod
+} 
+
+
+let max = 20
+ function max (){
+Math.max(1,16,2,20);
+ };
